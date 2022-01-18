be89628d683a282bedc77f25b70545107f70db6f
vladpanfilov1998 <145s@ukr.net>
2022-01-18 10:44:45 +0200

    base

---
+++ src/components/Form/Form.js
@@ -0,0 +1,28 @@
+import {useState} from "react";
+
+const Form = ({getFilter}) => {
+    const [form, setForm] = useState({name:'', username:'', email:''});
+
+    const formHandler = (e) => {
+        const eventData = {...form, [e.target.name]: e.target.value}
+        setForm({...form, ...eventData})
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        getFilter(form)
+    }
+
+    return (
+        <div>
+            <form>
+                <label>Name: <input type="text" name={'name'} value={form.name} onChange={formHandler}/></label>
+                <label>Username: <input type="text" name={'username'} value={form.username} onChange={formHandler}/></label>
+                <label>email: <input type="text" name={'email'} value={form.email} onChange={formHandler}/></label>
+                <button onClick={onSubmit}>Filter</button>
+            </form>
+        </div>
+    );
+};
+
+export default Form;
---
+++ src/services/user.service.js
@@ -0,0 +1,8 @@
+import {axiosService} from "./axios.service"
+import {urls} from "../configs/urls";
+
+export const userService = {
+    getAll:()=>axiosService.get(urls.users).then(value => value.data)
+}
+
+export default userService;
